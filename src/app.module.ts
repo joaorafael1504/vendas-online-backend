010138d6170810f8121eb0593b631ee25be115c1
João Rafael <jrafael1504@gmail.com>
2024-09-16 16:56:14 -0300

    remove imports

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,10 +1,10 @@
 import { Module } from '@nestjs/common';
-import { AppController } from './app.controller';
-import { AppService } from './app.service';
+import { UserModule } from './user/user.module';
+import { UsearController } from './usear/usear.controller';
 
 @Module({
-  imports: [],
-  controllers: [AppController],
-  providers: [AppService],
+  imports: [UserModule],
+  controllers: [UsearController],
+  providers: [],
 })
 export class AppModule {}
